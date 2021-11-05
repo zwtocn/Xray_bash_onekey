c7532d688e152cd550b073048610304b88d4b5f1
zwtocn <lucjym@hotmail.com>
2021-11-06 03:14:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -81,9 +81,6 @@ check_system() {
         echo -e "${OK} ${GreenBG} 当前系统为 Centos ${VERSION_ID} ${VERSION} ${Font}"
         INS="yum"
         [[ ! -f ${xray_qr_config_file} ]] && $INS update
-    elif [[ "${ID}" == "ol" && ${VERSION_ID} -ge 7.9 ]]; then
-        ##echo -e "${OK} ${GreenBG} 当前系统为 ol ${VERSION_ID} ${VERSION} ${Font}"
-        INS="yum"
         [[ ! -f ${xray_qr_config_file} ]] && $INS update
     elif [[ "${ID}" == "debian" && ${VERSION_ID} -ge 8 ]]; then
         echo -e "${OK} ${GreenBG} 当前系统为 Debian ${VERSION_ID} ${VERSION} ${Font}"
